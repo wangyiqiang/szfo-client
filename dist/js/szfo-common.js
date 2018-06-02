af2008892b6a1165a8d9972c4dde3ac6376539b6
unknown <jack.wangyq@gmail.com>
2018-06-02 20:06:40 +0800

    public page commit

--- dist/js/szfo-common.js
+++ dist/js/szfo-common.js
@@ -191,3 +191,17 @@ function genFeedbackEL(feedback){
     }
     return fbEl;
 }
+
+/**
+ * 手机号码验证
+ * @param str
+ * @returns {boolean}
+ */
+function checkPhoneNum(str) {
+    var myreg=/^[1][3,4,5,7,8][0-9]{9}$/;
+    if (!myreg.test(str)) {
+        return false;
+    } else {
+        return true;
+    }
+}
